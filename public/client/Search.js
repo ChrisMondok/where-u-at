dba96921064af5e2418f39c122b7b471290f1943
Chris Mondok <chris.mondok@gmail.com>
2016-05-13 23:26:13 -0400

    Switch from marker to infowindow on search

--- public/client/Search.js
+++ public/client/Search.js
@@ -1,8 +1,7 @@
 function Search(map) {
 	this.map = map
 
-	this.marker = new google.maps.Marker({
-		map: map,
+	this.infoWindow = new google.maps.InfoWindow({
 		visible: false
 	})
 
@@ -29,23 +28,63 @@ Search.prototype.makeAutocomplete = function () {
 
 	autocomplete.addListener('place_changed', function() {
 		var place = autocomplete.getPlace()
+		if(place.geometry)
+			this.result = place
+		else
+			this.result = null
+		
+	}.bind(this))
+
+	return input
+}
+
+Object.defineProperty(Search.prototype, 'result', {
+	get: function() {
+		return this._result
+	},
+	set: function(place) {
+		this._result = place
+
+		this.updateInfoWindow()
+
 		if(place.geometry) {
-			this.marker.setIcon({
-				url: place.icon,
-				size: new google.maps.Size(71, 71),
-				scaledSize: new google.maps.Size(35, 35)
-			})
-			this.marker.setVisible(true)
-			this.marker.setPosition(place.geometry.location)
-			this.marker.setAnimation(google.maps.Animation.DROP)
-			if(place.geometry.viewport) this.map.fitBounds(place.geometry.viewport)
+			this.infoWindow.open(this.map)
+
+			if(place.geometry.viewport)
+				this.map.fitBounds(place.geometry.viewport)
 			else {
 				this.map.setCenter(place.geometry.location)
 				this.map.setZoom(17)
 			}
 		}
-		
-	}.bind(this))
+	}
+})
 
-	return input
+Search.prototype.updateInfoWindow = function() {
+	if(!this.result || !this.result.geometry) {
+		this.infoWindow.close()
+		return
+	}
+
+	var content = document.createElement('div')
+
+	if(this.result.name) {
+		var name = document.createElement('div')
+		name.textContent = this.result.name
+		content.appendChild(name)
+	}
+
+	if(this.result.formatted_address) {
+		var address = document.createElement('address')
+		address.textContent = this.result.formatted_address
+		content.appendChild(address)
+	}
+
+	var button = document.createElement('button')
+	button.textContent = 'Set target'
+	content.appendChild(button)
+
+	this.infoWindow.setContent(content)
+
+	this.infoWindow.setPosition(this.result.geometry.location)
 }
